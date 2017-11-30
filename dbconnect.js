e5e64a223f09eff4e02dbb222b232bb706d07d74
Erik Hellestol <31954581+ErikHellestol@users.noreply.github.com>
2017-11-30 20:00:36 +0100

    t
    tt

--- dbconnect.js
+++
@@ -1,7 +0,0 @@
-var pgp = require('pg-promise')();
-
-//db connect string
-var conn = process.env.DATABASE_URL || 'postgres://ybyncykjxyctqo:02d96d972d10c154bf3524ee74dbefa3543ebec61049e3db4f7eae9114956db0@ec2-46-137-174-67.eu-west-1.compute.amazonaws.com:5432/d34eo2gvf0u1tf';
-var db = pgp(conn);
-
-module.exports = db;
